71db21831a4e83c1a368c12debd7747771bcff7d
KohMat <KohMat@users.noreply.github.com>
2021-07-07 19:06:41 +0900

    Change json size to practical size

--- main.py
+++ main.py
@@ -3,31 +3,44 @@ import time
 
 import cysimdjson
 import hyperjson
+import numpy as np
 import orjson
 import rapidjson
 import simdjson
 import simplejson
 import ujson
+import os
 
-m = {
+print("# JSON Parser comparison", end="\n\n")
+
+data = {
     "timestamp": 1556283673.1523004,
-    "task_uuid": "0ed1a1c3-050c-4fb9-9426-a7e72d0acfc7",
-    "task_level": [1, 2, 1],
-    "action_status": "started",
-    "action_type": "main",
-    "key": "value",
-    "another_key": 123,
-    "and_another": ["a", "b"],
+    "images": np.random.rand(5, 3, 224, 224).tolist(),
+    "trajectories": np.random.rand(30, 5, 3, 8).tolist(),
 }
+
+test_times = 20
+
+file_path = "./test.json"
+with open(file_path, "w") as f:
+    json.dump(data, f, indent=4)
+
+file_size = os.path.getsize(file_path) / (1024 * 1024)
+
+print(
+    f"The elapsed durations for {test_times} operations (dump, load, loads) on a {file_size:.2f}MB json file or equivalent data.",
+    end="\n\n",
+)
 
 
 def dump_benchmark(name, dumps):
     start = time.time()
-    for _ in range(1000000):
-        dumps(m)
-    print(name, ":", time.time() - start, "[s]")
+    for _ in range(test_times):
+        dumps(data)
+    print(f"{name}: {time.time() - start:5f}[s]")
 
 
+print("```text")
 print("> Dump")
 dump_benchmark("json", json.dumps)
 # orjson only outputs bytes, but often we need unicode:
@@ -38,24 +51,52 @@ dump_benchmark("simdjson", simdjson.dumps)
 dump_benchmark("simplejson", simplejson.dumps)
 dump_benchmark("ujson", ujson.dumps)
 
-n = json.dumps(m)
+small_json = json.dumps(data)
 
 
-def load_benchmark(name, load):
+def loads_benchmark(name, loads):
     start = time.time()
-    for i in range(1000000):
-        load(n)
-    print(name, ":", time.time() - start, "[s]")
+    for i in range(test_times):
+        loads(small_json)
+    print(f"{name}: {time.time() - start:5f}[s]")
+
+
+print("> Loads")
+loads_benchmark("json", json.loads)
+loads_benchmark("orjson", orjson.loads)
+loads_benchmark("rapidjson", rapidjson.loads)
+loads_benchmark("hyperjson", hyperjson.loads)
+loads_benchmark(
+    "cysimdjson", lambda s: cysimdjson.JSONParser().parse(s.encode("utf-8"))
+)
+loads_benchmark("simdjson", simdjson.loads)
+loads_benchmark("simplejson", simplejson.loads)
+loads_benchmark("ujson", ujson.loads)
+
+
+def load_benchmark(name, load, use_loads=False):
+    start = time.time()
+    for i in range(test_times):
+        f = open(file_path, "r")
+        if not use_loads:
+            load(f)
+        else:
+            load(f.read())
+    print(f"{name}: {time.time() - start:5f}[s]")
 
 
 print("> Load")
-load_benchmark("json", json.loads)
-load_benchmark("orjson", orjson.loads)
-load_benchmark("rapidjson", rapidjson.loads)
-load_benchmark("hyperjson", hyperjson.loads)
+load_benchmark("json", json.load)
+load_benchmark("rapidjson", rapidjson.load)
+load_benchmark("hyperjson", hyperjson.load)
+load_benchmark("orjson", orjson.loads, use_loads=True)
 load_benchmark(
-    "cysimdjson", lambda s: cysimdjson.JSONParser().parse(s.encode("utf-8"))
+    "cysimdjson",
+    lambda s: cysimdjson.JSONParser().parse(s.encode("utf-8")),
+    use_loads=True,
 )
-load_benchmark("simdjson", simdjson.loads)
-load_benchmark("simplejson", simplejson.loads)
-load_benchmark("ujson", ujson.loads)
+load_benchmark("simdjson", simdjson.load)
+load_benchmark("simplejson", simplejson.load)
+load_benchmark("ujson", ujson.load)
+
+print("```")
--- readme.md
+++ readme.md
@@ -1,22 +1,32 @@
 # JSON Parser comparison
 
+The elapsed durations for 20 operations (dump, load, loads) on a 29.18MB json file or equivalent data.
+
 ```text
-$ python main.py
 > Dump
-json : 3.2870473861694336 [s]
-orjson : 0.5628752708435059 [s]
-rapidjson : 1.7613952159881592 [s]
-hyperjson : 1.381042718887329 [s]
-simdjson : 3.28767728805542 [s]
-simplejson : 4.896097183227539 [s]
-ujson : 1.1491625308990479 [s]
+json: 7.031142[s]
+orjson: 0.503097[s]
+rapidjson: 7.423390[s]
+hyperjson: 1.107033[s]
+simdjson: 6.821450[s]
+simplejson: 7.220219[s]
+ujson: 1.675329[s]
+> Loads
+json: 3.104644[s]
+orjson: 0.884602[s]
+rapidjson: 3.353113[s]
+hyperjson: 1.338105[s]
+cysimdjson: 0.631090[s]
+simdjson: 0.879798[s]
+simplejson: 3.221970[s]
+ujson: 1.736441[s]
 > Load
-json : 2.565263032913208 [s]
-orjson : 0.8250467777252197 [s]
-rapidjson : 1.7073149681091309 [s]
-hyperjson : 1.9152252674102783 [s]
-cysimdjson : 0.5973670482635498 [s]
-simdjson : 1.3114981651306152 [s]
-simplejson : 3.0985052585601807 [s]
-ujson : 1.136885166168213 [s]
+json: 3.762387[s]
+rapidjson: 6.340086[s]
+hyperjson: 2.019569[s]
+orjson: 1.551531[s]
+cysimdjson: 1.044431[s]
+simdjson: 1.395589[s]
+simplejson: 3.876986[s]
+ujson: 2.389583[s]
 ```
